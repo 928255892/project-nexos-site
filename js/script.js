f1417b6fb61d0d78466ca25106ff513efed19083
928255892 <jorgedomingos457@gmail.com>
2025-04-09 00:31:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ btnTopo.addEventListener('click', () => {
 
 // Inicializando AOS com maior velocidade de animação
 AOS.init({
-  duration: 500, // Duração de 500ms para uma animação mais rápida
+  duration: 500, // Duração de 300ms para uma animação mais rápida
   easing: 'ease-in-out',
 });
 
