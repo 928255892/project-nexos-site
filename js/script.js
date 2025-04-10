950460f84cbb61b1ec5c6770ccc3a7eb87555773
928255892 <jorgedomingos457@gmail.com>
2025-04-10 02:04:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -61,33 +61,3 @@ if (logo) {
     }
   });
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  // AOS Initialization
-  AOS.init({
-    once: true,
-  });
-
-  // Menu Mobile Toggle
-  const menuToggle = document.getElementById("menuToggle");
-  const menuMobile = document.getElementById("menuMobile");
-
-  menuToggle.addEventListener("click", () => {
-    menuMobile.classList.toggle("open");
-  });
-
-  // Botão Voltar ao Topo
-  const btnVoltarTopo = document.getElementById("btnVoltarTopo");
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-      btnVoltarTopo.style.display = "block";
-    } else {
-      btnVoltarTopo.style.display = "none";
-    }
-  });
-
-  btnVoltarTopo.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-});
