c8e9bef4bdc8b2cb44e5017986fadf9c61797a08
928255892 <jorgedomingos457@gmail.com>
2025-04-12 20:40:12 +0100

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const User = require('../models/User'); // Certifique-se de que o modelo de User está configurado
+const bcrypt = require('bcryptjs'); // ← Aqui está a correção
+const User = require('../models/User');
 
 // Rota para cadastro
 router.post('/cadastro', async (req, res) => {
