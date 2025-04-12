6100c0ef82ce6d7bd4a2339522d65d2d2b36fac0
928255892 <jorgedomingos457@gmail.com>
2025-04-12 13:44:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-MONGO_URI=mongodb+srv://<jorgedomingos457>:<PMR7D50uXfGX8MEa>@project-nexos-cluster.mongodb.net/nexosdb?retryWrites=true&w=majority
-JWT_SECRET=sua-chave-ultra-secreta
-PORT=5000
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB', err));
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
