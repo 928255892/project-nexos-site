d5d059f987a040d6c9530744a04deec6ae826cac
928255892 <jorgedomingos457@gmail.com>
2025-04-14 10:14:11 +0100

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,7 +10,7 @@ module.exports = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-senha');
+    req.user = await User.findById(decoded.userId).select('-senha');
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token inválido.' });
